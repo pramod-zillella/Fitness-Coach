6061ae3498617d07612761d317727cf5d16b156b
Pramod Zillella <zillellapramod@gmail.com>
2024-09-12 03:12:50 -0400

    stremlit_option_menu removed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import time
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 from dotenv import load_dotenv
 from pinecone import Pinecone
 from sentence_transformers import SentenceTransformer
